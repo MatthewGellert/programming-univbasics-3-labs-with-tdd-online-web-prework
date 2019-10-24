22de24bb9d84439e427310f7e2288079dfd36bc3
Matthew Gellert <kifireguy@gmail.com>
2019-10-24 21:10:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,11 @@
-first_number = 6
-Second_number = 0
+# File: spec/calculator_spec.rb
+require './calculator.rb'
+describe "calculator" do
+it 'adds numbers' do
+    ...
+  end
+it 'subtracts numbers' do
+    calc = Calculator.new
+    expect(calc.subract(5,1)).to eql(4)
+  end
+end
